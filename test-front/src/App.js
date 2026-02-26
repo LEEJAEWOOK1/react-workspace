c73345baf0356684a636a7c1d9d662af42a84e2e
leejaewook <leee913@naver.com>
2026-02-26 18:07:30 +0900

    AWS 배포 테스트

---
+++ test-front/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+const path = "http://43.203.222.16:8080/"
+function App() {
+  const [data, setData] = useState()
+  useEffect( ()=> {
+    const dataFunc = async () => {
+      const res = await fetch(path);
+      if( res.ok ){
+        //boot 결과 값이 key,value형식이 아니므로 text()처리
+        const data = await res.text();
+        setData(data);
+      }
+    }
+    dataFunc();
+  })
+  return (<>
+    <h3>REACT</h3><h3>boot ip : {data}</h3>
+    </>
+  );
+}
+export default App;
